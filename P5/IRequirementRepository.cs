b33823804910f4fa018c05a7975b444a59dcd58b
Shawn Ensz <Shawn.Ensz@trojans.dsu.edu>
2021-11-29 23:00:32 -0600

    Created interface IRequirementRepository

---
+++ P5/IRequirementRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public interface IRequirementRepository
+    {
+        string Add(Requirement requirement);
+        List<Requirement> GetAll(int projectId);
+        string Remove(Requirement requirement);
+        string Modify(Requirement requirement);
+        Requirement GetRequirementById(int projectId, int requirementId);
+        int CountByFeature(int featureId);
+        void RemoveByFeatureId(int featureId);
+    }
+}
