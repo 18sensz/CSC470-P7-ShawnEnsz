8e8d5532d479402263eb49bbdc55654445a83e76
Shawn Ensz <Shawn.Ensz@trojans.dsu.edu>
2021-11-29 22:54:04 -0600

    Finished implementing FakeFeatureRepository methods

--- P5/FakeFeatureRepository.cs
+++ P5/FakeFeatureRepository.cs
@@ -8,29 +8,76 @@ namespace P5
 {
     public class FakeFeatureRepository : IFeatureRepository
     {
+        public string NO_ERROR = "";
+        public string DUPLICATE_TITLE_ERROR = "Title must be unique.";
+        public string EMPTY_TITLE_ERROR = "Title must have a value.";
+        public string NOT_FOUND_ERROR = "Feature not found.";
+        public string INVALID_PROJECT_ID_ERROR = "Invalid Project Id for Feature.";
+
+        private static List<Feature> features = new List<Feature>();
+
+
         public string Add(Feature feature)
         {
-
+            if (GetFeatureByTitle(feature.ProjectId, feature.Title) != null)
+                return DUPLICATE_TITLE_ERROR;
+            if (string.IsNullOrEmpty(feature.Title))
+                return EMPTY_TITLE_ERROR;
+            
+            feature.Id = features.Count + 1;
+            features.Add(feature);
+            return NO_ERROR;
         }
         public List<Feature> GetAll(int projectId)
         {
-
+            List<Feature> returnList = new List<Feature>();
+            foreach(Feature feature in features)
+            {
+                if (feature.ProjectId == projectId)
+                    returnList.Add(feature);
+            }
+            return returnList;
         }
         public string Remove(Feature feature)
         {
-
+            var featureToRemove = features.FindIndex(r => r.Id == feature.Id);
+            if (featureToRemove == -1)
+                return NOT_FOUND_ERROR;
+            features.RemoveAt(featureToRemove);
+            return NO_ERROR;
         }
         public string Modify(Feature feature)
         {
+            var featureToModify = features.FindIndex(r => r.Id == feature.Id);
+            if (featureToModify == -1)
+                return NOT_FOUND_ERROR;
 
+            features.RemoveAt(featureToModify);
+            if (GetFeatureByTitle(feature.ProjectId, feature.Title) != null)
+                return DUPLICATE_TITLE_ERROR;
+            if (string.IsNullOrEmpty(feature.Title))
+                return EMPTY_TITLE_ERROR;
+
+            features.Add(feature);
+            return NO_ERROR;
         }
         public Feature GetFeatureById(int projectId, int featureId)
         {
+            List<Feature> searchList = new List<Feature>();
+            searchList = GetAll(projectId);
 
+            var featureToReturn = features.FindIndex(r => r.Id == featureId);
+
+            return features[featureToReturn];
         }
         public Feature GetFeatureByTitle(int projectId, string title)
         {
+            List<Feature> searchList = new List<Feature>();
+            searchList = GetAll(projectId);
 
+            var featureToReturn = features.FindIndex(r => r.Title == title);
+
+            return features[featureToReturn];
         }
     }
 }
