f6c49e2498a17c1b29f5b170cb71960d7030b3ac
Shawn Ensz <Shawn.Ensz@trojans.dsu.edu>
2021-11-29 22:29:28 -0600

    Created two new classes Requirments and Features.
    Created and Implemented the FeatureRepository

---
+++ P5/FakeFeatureRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5
+{
+    public class FakeFeatureRepository : IFeatureRepository
+    {
+        public string Add(Feature feature)
+        {
+
+        }
+        public List<Feature> GetAll(int projectId)
+        {
+
+        }
+        public string Remove(Feature feature)
+        {
+
+        }
+        public string Modify(Feature feature)
+        {
+
+        }
+        public Feature GetFeatureById(int projectId, int featureId)
+        {
+
+        }
+        public Feature GetFeatureByTitle(int projectId, string title)
+        {
+
+        }
+    }
+}
